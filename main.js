90fda6f7dc64665abad69ae70247dfcb76e3d35b
helolhelol79-arch <helolhelol79@gmail.com>
2026-09-22 17:27:11 +0600

    end

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ let menu = document.querySelector(".fixed-menu");
 menu.addEventListener('click', function(event) {
     let clickTarget = event.target;
     let activeBtn = document.querySelector('.active');
+    let body = document.querySelector('body')
+    let mobileMenu = document.querySelector('.mobile-menu')
+    if (!body.classList.contains("off-scroll") &&
+!mobileMenu.classList.contains('hide') ) {
+body.classList.add("off-scroll");
+}
+else{
+body.classList.remove("off-scroll");
+}
     
     if (clickTarget.classList.contains('nav-link')) {
         clickTarget.classList.add('active');
